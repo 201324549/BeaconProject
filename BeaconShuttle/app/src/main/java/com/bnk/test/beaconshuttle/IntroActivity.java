e4f8055db1c55160ada14f24dd9b57d122876ab4
201324549 <god85a@naver.com>
2020-12-23 13:11:28 +0900

    BeaconProject Start

---
+++ BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/IntroActivity.java
@@ -0,0 +1,34 @@
+package com.bnk.test.beaconshuttle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.what == 1){
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
+
+
+}
---
+++ BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/LoginActivity.java
@@ -0,0 +1,119 @@
+package com.bnk.test.beaconshuttle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText loginId, loginPassword;
+    CheckBox autoLogin, saveId;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Boolean loginChecked;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginId = (EditText) findViewById(R.id.loginId);
+        loginPassword = (EditText) findViewById(R.id.loginPassword);
+        autoLogin = (CheckBox) findViewById(R.id.autoLogin);
+        saveId = (CheckBox) findViewById(R.id.saveId);
+        pref = getSharedPreferences("login_info", 0);
+        editor = pref.edit();
+        loginId.setText(pref.getString("id", ""));
+        saveId.setChecked(pref.getBoolean("saveId", false));
+
+
+        loginChecked = pref.getBoolean("autoLogin", false);
+        if(!loginChecked) {
+            loginId.setText(pref.getString("id", ""));
+            autoLogin.setChecked(false);
+        } else {
+            String id = loginId.getText().toString();
+            String password = loginPassword.getText().toString();
+            Boolean validation = loginValidation(id, password);
+
+            if(validation){
+                Toast.makeText(LoginActivity.this, "자동 로그인중", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    loginChecked = true;
+                } else {
+                    // if unChecked, removeAll
+                    loginChecked = false;
+                    editor.clear();
+                    editor.apply();
+                }
+            }
+        });
+
+    }
+
+    public void loginClick(View v){
+        Boolean check = loginValCheck(loginId.getText().toString(), loginPassword.getText().toString());
+        if(check) {
+            if(autoLogin.isChecked()){
+                editor.putString("id", loginId.getText().toString());
+                editor.putString("password", loginPassword.getText().toString());
+                editor.putBoolean("autoLogin", true);
+                editor.apply();
+            } else {
+                editor.clear();
+                editor.apply();
+            }
+
+            if(saveId.isChecked()) {
+                editor.putString("id", loginId.getText().toString());
+                editor.putBoolean("saveId", true);
+                editor.apply();
+
+            }
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(v.getContext(), "아이디 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean loginValCheck(String id, String password) {
+        if(id.equals("") || password.equals("")){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean loginValidation(String id, String password) {
+        if(pref.getString("id", "").equals(id) && pref.getString("pw", "").equals(password)){
+            return true;
+        } else if (pref.getString("id", "").equals(null)){
+            Toast.makeText(LoginActivity.this, "Please Sing in first", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+}
