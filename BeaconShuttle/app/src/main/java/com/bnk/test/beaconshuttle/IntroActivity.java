3a448d38647780a21eb6085f24a829617908b97b
201324549 <32638682+201324549@users.noreply.github.com>
2020-12-28 08:56:32 +0900

    Revert "Jingi"

--- BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/IntroActivity.java
+++ BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/IntroActivity.java
@@ -5,12 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
-import com.bumptech.glide.Glide;
 
 public class IntroActivity extends Activity {
 
@@ -18,8 +15,7 @@ public class IntroActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        Glide.with(this).load(R.raw.bus).into(imageView);
+
         IntroThread introThread = new IntroThread(handler);
         introThread.start();
     }
--- BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/OnViewHolderItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.bnk.test.beaconshuttle;
-
-public interface OnViewHolderItemClickListener {
-    void onViewHolderItemClick();
-}
