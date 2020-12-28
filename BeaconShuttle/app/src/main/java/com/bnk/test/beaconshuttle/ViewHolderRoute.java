232237ebad745ab76b8111f6bb8821627b15f7af
201324549 <god85a@naver.com>
2020-12-28 12:24:43 +0900

    route db connection

--- BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/ViewHolderRoute.java
+++ BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/ViewHolderRoute.java
@@ -1,8 +1,12 @@
 package com.bnk.test.beaconshuttle;
 
 import android.animation.ValueAnimator;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,21 +14,23 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bnk.test.beaconshuttle.model.DataRoute;
+import com.bnk.test.beaconshuttle.model.DataStop;
+import com.bnk.test.beaconshuttle.util.DataHelper;
+import com.bnk.test.beaconshuttle.util.MyApp;
 
 public class ViewHolderRoute extends RecyclerView.ViewHolder {
     TextView rot_nm;
     TextView testtext;
     LinearLayout linearlayout;
-
+    LinearLayout stoplist;
+    DataHelper dh;
     OnViewHolderItemClickListener onViewHolderItemClickListener;
-
     public ViewHolderRoute(@NonNull View itemView) {
         super(itemView);
 
         rot_nm = itemView.findViewById(R.id.rot_nm);
-        testtext = itemView.findViewById(R.id.testtext);
         linearlayout = itemView.findViewById(R.id.linearlayout);
-
+        stoplist = itemView.findViewById(R.id.stoplist);
         linearlayout.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -37,18 +43,29 @@ public class ViewHolderRoute extends RecyclerView.ViewHolder {
 
     public void onBind(DataRoute data, int position, SparseBooleanArray selectedItems) {
         rot_nm.setText(data.getRot_nm());
-        testtext.setText(data.getRot_id()+"");
+        stoplist.removeAllViews();
+        for(DataStop ds : data.getListStop()){
+            TextView view1 = new TextView(MyApp.getAppContext());
+            view1.setText(ds.getStop_nm());
+            TextView view2 = new TextView(MyApp.getAppContext());
+            view2.setText(ds.getStar_time());
+            stoplist.addView(view1);
+            stoplist.addView(view2);
+
+
+        }
         changeVisibility(selectedItems.get(position));
     }
 
     private void changeVisibility(final boolean isExpanded){
-        ValueAnimator va = isExpanded ? ValueAnimator.ofInt(0, 200) : ValueAnimator.ofInt(200,0);
+        ValueAnimator va = isExpanded ? ValueAnimator.ofInt(0, 600) : ValueAnimator.ofInt(600,0);
         va.setDuration(500);
         va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
 
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                testtext.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+                stoplist.requestLayout();
+                stoplist.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
             }
         });
         va.start();
@@ -57,4 +74,5 @@ public class ViewHolderRoute extends RecyclerView.ViewHolder {
     public void setOnViewHolderItemClickListener(OnViewHolderItemClickListener onViewHolderItemClickListener) {
         this.onViewHolderItemClickListener = onViewHolderItemClickListener;
     }
+
 }
