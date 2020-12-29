20109ee24b9486ded207a47eb1252e6a3b52c875
201324549 <god85a@naver.com>
2020-12-29 17:57:40 +0900

    db & stop image popup update

--- BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/model/DataRoute.java
+++ BeaconShuttle/app/src/main/java/com/bnk/test/beaconshuttle/model/DataRoute.java
@@ -1,14 +1,54 @@
 package com.bnk.test.beaconshuttle.model;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import java.util.ArrayList;
 
 public class DataRoute {
     private int rot_id;
     private String rot_nm;
     private ArrayList<DataStop> listStop;
+    private Context ctx;
+    private LayoutInflater inflater;
+    private View v;
+    private Activity activity;
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
 
     public DataRoute(){
         this.listStop = new ArrayList<>();
+    }
+
+    public void setCtx(Context ctx){
+        this.ctx = ctx;
+    }
+    public void setV(View v){
+        this.v = v;
+    }
+
+    public View getV(){
+        return this.v;
+    }
+
+    public void setInflater(LayoutInflater inflater){
+        this.inflater = inflater;
+    }
+
+    public LayoutInflater getInflater(){
+        return inflater;
+    }
+
+    public Context getCtx(){
+        return this.ctx;
     }
 
     public void setRot_id(int rot_id) {
